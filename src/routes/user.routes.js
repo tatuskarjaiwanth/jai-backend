e54a34847ce21fda99a886c28463594da8ddf13c
TJaiwanth3 <jaiwanthtatuskar2@gmail.com>
2025-06-18 23:00:48 +0530

    add secured routes for changing password, retrieving current user, updating account details, and managing user avatar/cover image

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,9 +4,18 @@ import {
   logoutUser,
   registerUser,
   refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccoutDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
 } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middelware.js";
 import {verifyJWT} from "../middlewares/auth.middleware.js";
+import {verify} from "crypto";
+import {get} from "http";
 const router = Router();
 
 router.route("/register").post(
@@ -30,4 +39,18 @@ router.route("/login").post(loginUser);
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-accoubt").patch(verifyJWT, updateAccoutDetails);
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/coverImage")
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+
+router.route("/history").get(verifyJWT, getWatchHistory);
+
 export default router;
